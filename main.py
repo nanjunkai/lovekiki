ba58775786ed31d44d50af46b3170421373d71f9
nanjunkai <48474704+nanjunkai@users.noreply.github.com>
2022-08-22 17:38:08 +0800

    Update main.py
    同时推送两个人

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
 user_id = os.environ["USER_ID"]
+user_id_2 = os.environ["USER_ID_2"]
 template_id = os.environ["TEMPLATE_ID"]
 
 
@@ -49,5 +50,7 @@ client = WeChatClient(app_id, app_secret)
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
-res = wm.send_template(user_id, template_id, data)
-print(res)
+res1 = wm.send_template(user_id, template_id, data)
+res2 = wm.send_template(user_id_2, template_id, data)
+print(res1)
+print(res2)
